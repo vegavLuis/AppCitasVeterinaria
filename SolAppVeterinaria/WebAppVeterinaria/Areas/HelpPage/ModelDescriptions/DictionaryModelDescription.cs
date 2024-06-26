63db91b760e7f07dde844e7153b0541e0d303e12
Luis Vegea <vegalu63@gmail.com>
2024-06-26 00:28:10 -0600

    git init

---
+++ SolAppVeterinaria/WebAppVeterinaria/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAppVeterinaria.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SolAppVeterinaria/WebAppVeterinaria/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAppVeterinaria.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
